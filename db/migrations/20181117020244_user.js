fe6c661a39b2108ec80b21ad555a782afa1c3f26
= <peterhjhan@gmail.com>
2018-11-17 02:36:29 +0000

    create connection table

--- db/migrations/20181117020244_user.js
+++ db/migrations/20181117020244_user.js
@@ -4,7 +4,9 @@ exports.up = function(knex, Promise) {
     table.increments('id').primary();
     table.string('first_name').notNull();
     table.string('qrcode').notNull();
+    table.dateTime('create_date').notNull();
     table.string(['question', 'answer']).notNull();
+    
   })
 };
 
--- db/migrations/20181117022529_connection.js
+++ db/migrations/20181117022529_connection.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('connections', (table) =>  {
+    table.increments('id').primary();
+    table.integer('first_user_id').unsigned().notNull();
+    table.integer('second').unsigned().notNull();
+    table.dateTime('expire_at')
+    table.dateTime('friends_at')
+    table.string(['question', 'answer']).notNull();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('connections');
 };
