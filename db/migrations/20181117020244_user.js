b856d564f90b5ab734ca4af4cd41faf901d40b9b
= <peterhjhan@gmail.com>
2018-11-17 02:26:09 +0000

    update knex and user migration

---
+++ db/migrations/20181117020244_user.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) =>  {
+    table.increments('id').primary();
+    table.string('first_name').notNull();
+    table.string('qrcode').notNull();
+    table.string(['question', 'answer']).notNull();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
