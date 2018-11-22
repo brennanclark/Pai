a310fa680b0dfbac206f592e42e2d68491cf7064
= <peterhjhan@gmail.com>
2018-11-22 00:36:25 +0000

    distance between two complete

--- db/migrations/20181121202312_locations.js
+++ db/migrations/20181121202312_locations.js
@@ -4,15 +4,13 @@ exports.up = function(knex, Promise) {
     table.integer('user_id').unsigned().notNull();
     table.unique('user_id');
     table.index('user_id');
-    table.float('lat', 14, 10);
-    table.float('long', 14, 10);
+    table.decimal('lat', 14 , 10);
+    table.decimal('long',14 , 10);
     table.dateTime('last_check_in');
 
     table.foreign('user_id').references('id').inTable('users');
   })
- 
  };
- 
  exports.down = function(knex, Promise) {
   return knex.schema.dropTable('locations');
  };
